e2fc2a339f451cbed365f925d5f304ff56c7d661
arealclimber <luffulgence@gmail.com>
2022-07-20 15:23:26 +0800

    rainbow-kit

--- src/sections/Mint.js
+++ src/sections/Mint.js
@@ -1,19 +1,28 @@
 import Button from '../components/Button';
+import { ConnectButton } from '@rainbow-me/rainbowkit';
 
 const Mint = () => {
 	return (
-		<section className="mt-16 flex flex-col item-center justify-center">
-			<h1 className="text-7xl font-bold font-forHeading">
-				Mint Your{' '}
-				<span className="dark:text-blue-200 text-red-900">NFT</span>
-			</h1>
-			<h3 className="text-4xl my-3">On Rinkeby Testnet</h3>
-			<p className="text-gray-600 mb-8">
-				Randomized picture powered by hashlip
-			</p>
-			<Button className="text-5xl px-10 py-5 bg-blue-900 text-white mt-20">
-				Mint
-			</Button>
+		<section className="mt-16 flex flex-col justify-center">
+			<div className="flex justify-center mb-10 mx-5">
+				<ConnectButton />
+			</div>
+
+			<div className="flex flex-col items-center justify-center">
+				<h1 className="text-7xl font-bold font-forHeading">
+					Mint Your{' '}
+					<span className="dark:text-blue-200 text-red-900">NFT</span>
+				</h1>
+				<h3 className="text-4xl my-3">On Rinkeby Testnet</h3>
+				<p className="text-gray-600 mb-8">
+					Randomized picture powered by hashlip
+				</p>
+			</div>
+			<div className="flex flex-col items-center justify-center">
+				<Button className="text-5xl px-10 py-5 bg-blue-900 text-white mt-20">
+					Mint
+				</Button>
+			</div>
 		</section>
 	);
 };
